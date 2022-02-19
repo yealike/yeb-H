e9c35752e33a995cf7d701ce66abb5df2a554366
yeahan <925182704@qq.com>
2022-02-19 22:42:01 +0800

    第一天结束

--- yeb-server/src/main/java/com/example/service/IAdminService.java
+++ yeb-server/src/main/java/com/example/service/IAdminService.java
@@ -2,6 +2,9 @@ package com.example.service;
 
 import com.example.pojo.Admin;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.pojo.RespBean;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -13,4 +16,19 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IAdminService extends IService<Admin> {
 
+    /**
+     * 登录之后返回token
+     * @param username
+     * @param password
+     * @param request
+     * @return
+     */
+    RespBean login(String username, String password, HttpServletRequest request);
+
+    /**
+     * 根据用户名获取完整的用户
+     * @param username
+     * @return
+     */
+    Admin getAdminByUsername(String username);
 }
