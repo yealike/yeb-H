94bbf84b173ab860c508719997e5d78d4d1536c5
yehan <925182704@qq.com>
2022-02-24 19:20:24 +0800

    邮件服务相关配置

--- yeb-server/src/main/java/com/example/server/service/impl/EmployeeServiceImpl.java
+++ yeb-server/src/main/java/com/example/server/service/impl/EmployeeServiceImpl.java
@@ -3,20 +3,24 @@ package com.example.server.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.example.server.pojo.Employee;
+import com.example.server.mapper.MailLogMapper;
+import com.example.server.pojo.*;
 import com.example.server.mapper.EmployeeMapper;
-import com.example.server.pojo.RespBean;
-import com.example.server.pojo.RespPageBean;
 import com.example.server.service.IEmployeeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.DecimalFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * <p>
@@ -30,6 +34,10 @@ import java.util.Map;
 public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements IEmployeeService {
     @Autowired
     private EmployeeMapper employeeMapper;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private MailLogMapper mailLogMapper;
 
     /**
      * 获取所有员工（分页）
@@ -76,15 +84,36 @@ public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> i
         long days = beginContract.until(endContract, ChronoUnit.DAYS);
         DecimalFormat decimalFormat = new DecimalFormat("##.00");
         employee.setContractTerm(Double.parseDouble(decimalFormat.format(days / 365.00)));
-        if (1==employeeMapper.insert(employee)){
-            return RespBean.success("添加成功！" +
-                    "");
+        if (1 == employeeMapper.insert(employee)) {
+
+            Employee emp = employeeMapper.getEmployee(employee.getId()).get(0);
+
+            //数据库记录发送的消息
+            String msgId = UUID.randomUUID().toString();
+            MailLog mailLog = new MailLog();
+            mailLog.setMsgId(msgId);
+            mailLog.setEid(employee.getId());
+            mailLog.setStatus(0);
+            mailLog.setRouteKey(MailConstants.MAIL_ROUTING_KEY_NAME);
+            mailLog.setExchange(MailConstants.MAIL_EXCHANGE_NAME);
+            mailLog.setCount(MailConstants.MAX_TRY_COUNT);
+            mailLog.setTryTime(LocalDateTime.now().plusMinutes(MailConstants.MSG_TIMEOUT));
+            mailLog.setCreateTime(LocalDateTime.now());
+            mailLog.setUpdateTime(LocalDateTime.now());
+            mailLogMapper.insert(mailLog);
+
+            //发送邮件
+            rabbitTemplate.convertAndSend(MailConstants.MAIL_EXCHANGE_NAME,
+                    MailConstants.MAIL_ROUTING_KEY_NAME, emp, new CorrelationData(msgId));
+
+            return RespBean.success("添加成功！");
         }
         return RespBean.error("添加失败！");
     }
 
     /**
      * 查询员工
+     *
      * @param id
      * @return
      */
---
+++ yeb-server/src/main/java/com/example/server/task/MailTask.java
@@ -0,0 +1,7 @@
+package com.example.server.task;
+
+/**
+ * 邮件发送定时任务
+ */
+public class MailTask {
+}
